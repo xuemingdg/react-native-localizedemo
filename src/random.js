09002acb13237aa1f456a404b6f2c7b0962d0f8f
Xueming Deng <xuemingdg@outlook.com>
2020-06-09 16:03:01 +0800

    remove hooks

--- src/random.js
+++ src/random.js
@@ -7,7 +7,6 @@ import { useLanguageUpdate } from './hook/useLanguageUpdate';
 import 'react-native-gesture-handler';
 
 export default function RandomPage() {
-    useLanguageUpdate();
     return (
         <SafeAreaView style={{flex:1}}>
             <View style={{flex:1,
